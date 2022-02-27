0e3ef3c74687a94359b53d3cc46c1e34937eafdb
Ashique Mushtaq <ashiquemushtaqt@gmail.com>
2022-02-27 14:29:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const fetchData = async () => {
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const myJson = await response.json();
+    // const advice = myJson.slip.advice
+    document.getElementById("quote").innerText = myJson.slip.advice
+    document.getElementsByClassName("title")[0].innerHTML = "ADVICE #"+myJson.slip.id
+}
