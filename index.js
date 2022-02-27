757bcc44c53350b69cd69d45f65b9dad2abc004a
Ashique Mushtaq <ashiquemushtaqt@gmail.com>
2022-02-27 15:08:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fetchData = async () => {
     const response = await fetch('https://api.adviceslip.com/advice');
     const myJson = await response.json();
-    // const advice = myJson.slip.advice
     document.getElementById("quote").innerText = '"'+myJson.slip.advice+'"'
     document.getElementsByClassName("title")[0].innerHTML = "ADVICE #"+myJson.slip.id
 }
